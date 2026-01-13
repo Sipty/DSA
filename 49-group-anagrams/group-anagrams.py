25866c79a8dbd0f36bfd47f3360a0ca6287258c8
Chavdar Yotov <choko43@gmail.com>
2026-01-13 16:56:17 -0500

    Time: 1765 ms (5.02%) | Memory: 24 MB (6.13%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,32 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        ans = []
+        d_ans = [] # list of dict reprentations for each entry in ans
+
+
+        for s in strs:
+            
+            # convert string to dict
+            d = defaultdict(int)
+            for char in s:
+                d[char] += 1
+
+            
+            exists_in_d_ans = False
+            # check if exists in d_ans
+            for i in range(len(d_ans)):
+                if d == d_ans[i]:
+                    exists_in_d_ans = True
+                    ans[i].append(s)
+                    break
+
+            
+            # if not in d ans, add it, then separately to ans
+            if exists_in_d_ans is False:
+                d_ans.append(d)
+                ans.append([s])
+
+        return ans
+
+                
